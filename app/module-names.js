db3872dba727369252e2f84620ed2526e0fad14f
Ken Sin <ken.sin@ticketfly.com>
2017-01-27 13:29:05 -0800

    Update styles and refactor route

---
+++ app/module-names.js
@@ -0,0 +1,22 @@
+// List of css modules to fetch from Github
+
+export default [
+  'ticketfly-css-normalize',
+  'ticketfly-css-core-variables',
+  'ticketfly-css-box-shadow-variables',
+  'ticketfly-css-color-variables',
+  'ticketfly-css-spacing-variables',
+  'ticketfly-css-display-utilities',
+  'ticketfly-css-flex-utilities',
+  'ticketfly-css-overflow-utilities',
+  'ticketfly-css-sizing-utilities',
+  'ticketfly-css-spacing-utilities',
+  'ticketfly-css-text-utilities',
+  'ticketfly-css-z-index-utilities',
+  'ticketfly-css-box-objects',
+  'ticketfly-css-grid-objects',
+  'ticketfly-css-tap-target-objects',
+  'ticketfly-css-box-shadow-garnishes',
+  'ticketfly-css-color-garnishes',
+  'ticketfly-css-reset-garnishes'
+];
--- app/routes/available-css.js
+++ app/routes/available-css.js
@@ -1,48 +1,34 @@
 import Ember from 'ember';
 import fetch from 'fetch';
+import MODULE_NAMES from 'ticketfly-css-doc/module-names';
 
 const { RSVP } = Ember;
 
-const MODULE_NAMES = [
-  'ticketfly-css-normalize',
-  'ticketfly-css-core-variables',
-  'ticketfly-css-box-shadow-variables',
-  'ticketfly-css-color-variables',
-  'ticketfly-css-spacing-variables',
-  'ticketfly-css-display-utilities',
-  'ticketfly-css-flex-utilities',
-  'ticketfly-css-overflow-utilities',
-  'ticketfly-css-sizing-utilities',
-  'ticketfly-css-spacing-utilities',
-  'ticketfly-css-text-utilities',
-  'ticketfly-css-z-index-utilities',
-  'ticketfly-css-box-objects',
-  'ticketfly-css-grid-objects',
-  'ticketfly-css-tap-target-objects',
-  'ticketfly-css-box-shadow-garnishes',
-  'ticketfly-css-color-garnishes',
-  'ticketfly-css-reset-garnishes'
-];
-
 export default Ember.Route.extend({
   model() {
-    const cssFiles = MODULE_NAMES.map((name) => {
-      return this._fetchModule(name);
+    const modules = MODULE_NAMES.map(name => {
+      return this._fetchModule(name).then(body => {
+        return { name, body };
+      });
     });
 
-    return RSVP.all(cssFiles);
+    return RSVP.all(modules);
   },
 
   _fetchModule(name) {
     const url = this._githubURLFor(name);
-    return fetch(url).then((response) => {
+    return fetch(url).then(response => {
       return response.text();
-    }).then((body) => {
-      return { name, body};
     });
   },
 
   _githubURLFor(moduleName) {
     return `https://raw.githubusercontent.com/Ticketfly-UI/${moduleName}/master/index.css`;
+  },
+
+  actions: {
+    scrollTop() {
+      window.scrollTo(0, 0);
+    }
   }
 });
